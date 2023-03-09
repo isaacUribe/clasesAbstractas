a9440642b2f2558c2100e269798002b85f3e8d51
smarinv16 <funemesanchez@gmail.com>
2023-03-09 10:26:11 -0500

    se crea mensaje

--- src/main/java/org/example/PlanetaSayayin.java
+++ src/main/java/org/example/PlanetaSayayin.java
@@ -2,12 +2,16 @@ package org.example;
 
 public class PlanetaSayayin {
     public static void main(String[] args) {
+
+        System.out.println("Dragon ball");
         ClaseAlta guerrero1 = new ClaseAlta("vegeta",90,30,"negro");
         guerrero1.pelear();
 
 
         ClaseBaja guerrero2 = new ClaseBaja("goku",80,25,"negro");
         guerrero2.pelear();
+
+
 
     }
 }
